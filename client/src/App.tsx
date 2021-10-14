6d7da18a3c93ad0d4dd136ed95d2f9eab0412fc5
Eduard Cristian Tasca <eduardcristiantasca@gmail.com>
2021-10-14 22:38:03 +0300

    Basic routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import '../src/sass/main.scss';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Invoices from './pages/invoices/Invoices';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Props {
+
+}
+
+interface State {
+
+}
+
+class App extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <BrowserRouter>
+                    <main className="main-component">
+                        <Switch>
+                            <Route path="/" exact>
+                                <Invoices />
+                            </Route>
+                        </Switch>
+                    </main>
+                </BrowserRouter>
+            </div>
+        )
+    }
 }
 
 export default App;
