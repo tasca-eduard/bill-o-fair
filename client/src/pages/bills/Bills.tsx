3ce8fa4cea3b2c14cb26b00f119e98214a72b2b2
Eduard C. Tasca <eduardt@intranet.webwavers.net>
2021-10-21 18:41:03 +0300

    Table rendering with dummy data

--- client/src/pages/bills/Bills.tsx
+++ client/src/pages/bills/Bills.tsx
@@ -1,62 +1,118 @@
-import React from 'react';
+import React, { useRef, WheelEvent } from 'react';
+import dummyBillsData from '../../dummy-data/bills.json';
 
 interface Props {
 
 }
 
 interface State {
-
+    billsData: any
 }
 
 class Bills extends React.Component<Props, State> {
+    scrollContainer: React.RefObject<HTMLDivElement>;
+
     constructor(props: Props) {
         super(props);
+        this.scrollContainer = React.createRef();
+        this.state = {
+            billsData: dummyBillsData
+        }
+    }
+
+    onWheel = (e: WheelEvent) => {
+        e.preventDefault()
+        const container = this.scrollContainer.current;
+
+        if(container) {
+            const scrollContainerScrollPosition = container.scrollLeft;
+            container.scrollTo({
+                top: 0,
+                left: scrollContainerScrollPosition + e.deltaY,
+            })
+        }
+    }
+
+    componentDidMount() {
+        console.log(this.state.billsData)
     }
 
     render() {
         return (
-            <table className="bills-component">
-                <thead className="table-head">
-                    <tr className="row">
-                        <th className="head group">
-                            <div className="image-wrapper">
-                                <img className="group-image" src="https://c.pxhere.com/photos/6b/a1/apartment_building_balconies_building_balcony_apartment_architecture_home_housing-825619.jpg!s1" alt="User group" />
-                            </div>
-                            <div className="group-info">
-                                <h2 className="title">Crinului 7</h2>
-                                <h3 className="subtitle"> 
-                                    18.03.2021
-                                </h3>
-                            </div>
-                        </th>
-                        <th className="head">
-                            Heat 
-                            <i className="bill-icon fas fa-fire"></i>
-                        </th>
-                        <th className="head">
-                            Electricity
-                            <i className="bill-icon fas fa-bolt"></i>
-                        </th>
-                        <th className="head">
-                            Internet
-                            <i className="bill-icon fas fa-wifi"></i>
-                        </th>
-                        <th className="head">
-                            Water
-                            <i className="bill-icon fas fa-tint"></i>
-                        </th>
-                        <th className="head">
-                            Other
-                        </th>
-                        <th className="head total">
-                            Total
-                            <i className="bill-icon fas fa-money-bill"></i>
-                        </th>
-                    </tr>
-                </thead>
-                <tbody className="table-body">
-                </tbody>
-            </table>
+            <div className="shadow-mask right">
+                <div className="mask"></div>
+                <div className="bills-component" onWheel={this.onWheel} ref={this.scrollContainer}>
+                    <table className="bills-table">
+                        <thead className="table-head">
+                            <tr className="row">
+                                <th className="head">
+                                    <div className="group">
+                                        <div className="image-wrapper">
+                                            <img className="group-image" src={this.state.billsData.groupPhoto} alt="User group" />
+                                        </div>
+                                        <div className="group-info">
+                                            <h2 className="title">{this.state.billsData.groupName}</h2>
+                                            <h3 className="subtitle"> 
+                                                18.03.2021
+                                            </h3>
+                                        </div>
+                                    </div>
+                                </th>
+                                {this.state.billsData.services.map((service: any) => {
+                                    return (
+                                        <th className="head">
+                                            {service.name}
+                                            <i className={`bill-icon ${service.icon}`}></i>
+                                        </th>
+                                    )
+                                })}
+                                <th className="head total">
+                                    Total
+                                    <i className="bill-icon fas fa-money-bill"></i>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody className="table-body">
+                            {
+                                this.state.billsData.userBillData.map((userData: any) => {
+                                    return (
+                                        <tr className="row">
+                                            <th className="head user">
+                                                <div className="group">
+                                                    <img className="group-image" src={userData.photo} alt="User group" />
+                                                    <div className="group-info">
+                                                        <h2 className="title">{userData.name}</h2>
+                                                        <h3 className={`subtitle ${userData.paid ? 'paid' : 'unpaid'}`}> 
+                                                            {userData.paid ?
+                                                                <React.Fragment>
+                                                                    Paid 
+                                                                    <i className="status-icon fas fa-check"></i>
+                                                                </React.Fragment>
+                                                                :
+                                                                'Unpaid'
+                                                            }
+                                                        </h3>
+                                                    </div>
+                                                </div>
+                                            </th>
+                                            {userData.payments.map((payment: any) => {
+                                                return (
+                                                    <th className="cell">
+                                                        {payment.amount}
+                                                    </th>
+                                                )
+                                            })}
+                                            <th className="cell total">
+                                                {userData.totalAmount}
+                                            </th>
+                                        </tr>   
+                                    )
+                                })
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         )
     }
 }
