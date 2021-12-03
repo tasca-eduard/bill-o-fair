c866fd6eafb5136013b9883aaafbdb80e5cedb59
Eduard Tasca <eduardcristiantasca@gmail.com>
2021-12-03 17:54:11 +0200

    Added portal for modal

--- client/src/components/bill-details/BillDetails.tsx
+++ client/src/components/bill-details/BillDetails.tsx
@@ -4,6 +4,7 @@ import Slider from "react-slick";
 import { RouteComponentProps } from 'react-router';
 import { ServiceDTO } from '../../common/dtos/Service';
 import { NavLink } from 'react-router-dom';
+import Modal from '../modal/Modal';
 
 interface Props extends RouteComponentProps {
     serviceDetails: ServiceDTO[]
@@ -46,46 +47,36 @@ class BillDetails extends React.Component<Props, State> {
         return this.props.serviceDetails[currentSlide].name.toLowerCase().replace(' ', '');
     }
 
-    goToBillsPage = () => {
-        this.props.history.push('/bills');
-    }
-
-    preventPropagation = (e: React.MouseEvent<HTMLDivElement>) => {
-        e.stopPropagation()
-    }
-
     render() {
         return (
-            <div className="modal-overlay" onClick={this.goToBillsPage}>
-                <div className="modal" onClick={this.preventPropagation}>
-                    <div className="cta-wrapper">
-                        <NavLink className="back-cta" to="/bills">
-                            Back To Bills
-                        </NavLink>
-                    </div>
-                    <Slider 
-                        {...this.sliderSettings}
-                        initialSlide={this.state.slideIndex}
-                    >
-                        {
-                            this.props.serviceDetails.map((service: ServiceDTO) => {
-                                return (
-                                    <div className="bill-details-component">
-                                        <div className="info">
-                                            <div className="details">
-                                                <h2 className="title">{service.name}</h2>
-                                            </div>
-                                            <div className="photo-wrapper">
-                                                <img className="photo" src={service.photo} alt="Bill Photo" />
-                                            </div>
+            <Modal {...this.props}>
+                <div className="cta-wrapper">
+                    <NavLink className="back-cta" to="/bills">
+                        Back To Bills
+                    </NavLink>
+                </div>
+                <Slider 
+                    {...this.sliderSettings}
+                    initialSlide={this.state.slideIndex}
+                >
+                    {
+                        this.props.serviceDetails.map((service: ServiceDTO, index: number) => {
+                            return (
+                                <div key={index} className="bill-details-component">
+                                    <div className="info">
+                                        <div className="details">
+                                            <h2 className="title">{service.name}</h2>
+                                        </div>
+                                        <div className="photo-wrapper scrollable">
+                                            <img className="photo" src={service.photo} alt="Bill Photo" />
                                         </div>
                                     </div>
-                                )
-                            })
-                        }
-                    </Slider>
-                </div>
-            </div>
+                                </div>
+                            )
+                        })
+                    }
+                </Slider>
+            </Modal>
         )
     }
 }
--- client/src/pages/bills/Bills.tsx
+++ client/src/pages/bills/Bills.tsx
@@ -26,102 +26,120 @@ class Bills extends React.Component<Props, State> {
 
     renderTableHead = () => {
         return (
-            <React.Fragment>
-                {this.state.billsData.services.map((service: ServiceDTO) => {
-                    return (
-                        <th className="head">
-                            <NavLink className="link" title={service.name} to={`${this.props.match.url}/${service.name.toLowerCase().replaceAll(' ', '')}`}>
-                                {service.name}
-                                <i className={`bill-icon ${service.icon}`}></i>
-                            </NavLink>
-                        </th>
-                    )
-                })}
-                <th className="head total">
-                    Total
-                    <i className="bill-icon fas fa-money-bill"></i>
-                </th>
-            </React.Fragment>
+            <thead className="table-head">
+                <tr className="row">
+                    <th className="head">
+                        <div className="group">
+                            <div className="image-wrapper">
+                                <img className="group-image" src={this.state.billsData.groupPhoto} alt="User group" />
+                            </div>
+                            <div className="group-info">
+                                <h2 className="title">{this.state.billsData.groupName}</h2>
+                                <h3 className="subtitle"> 
+                                    18.03.2021
+                                </h3>
+                            </div>
+                        </div>
+                    </th>
+                    {this.renderServiceLinks()}
+                    <th className="head total">
+                        Total
+                        <i className="bill-icon fas fa-money-bill"></i>
+                    </th>
+                </tr>
+            </thead>
         )
+    }
+
+    renderServiceLinks = () => {
+        return this.state.billsData.services.map((service: ServiceDTO, index: number) => {
+            return (
+                <th key={index} className="head">
+                    <NavLink className="link" title={service.name} to={`${this.props.match.url}/${service.name.toLowerCase().replaceAll(' ', '')}`}>
+                        {service.name}
+                        <i className={`bill-icon ${service.icon}`}></i>
+                    </NavLink>
+                </th>
+            )
+        })
+    }
+
+    renderTableBody = () => {
+        return (
+            <tbody className="table-body">
+                {
+                    this.state.billsData.userBillData.map((userData: UserBillDataDTO, index: number) => {
+                        return (
+                            <tr key={index} className="row">
+                                <th className="head user">
+                                    <div className="group">
+                                        <img className="group-image" src={userData.photo} alt="User group" />
+                                        <div className="group-info">
+                                            <h2 className="title">{userData.name}</h2>
+                                            <h3 className={`subtitle ${userData.paid ? 'paid' : 'unpaid'}`}> 
+                                                {userData.paid ?
+                                                    <React.Fragment>
+                                                        Paid 
+                                                        <i className="status-icon fas fa-check"></i>
+                                                    </React.Fragment>
+                                                    :
+                                                    'Unpaid'
+                                                }
+                                            </h3>
+                                        </div>
+                                    </div>
+                                </th>
+                                {this.renderPayments(userData.payments)}
+                                <th className="cell total">
+                                    {userData.totalAmount}
+                                </th>
+                            </tr>   
+                        )
+                    })
+                }
+            </tbody>
+        )
+    }
+
+    renderPayments = (payments: PaymentDTO[]) => {
+        return payments.map((payment: PaymentDTO, index: number) => {
+            return (
+                <th key={index} className="cell">
+                    {payment.amount}
+                </th>
+            )
+        })
+    }
+
+    renderBillRoutes = (serviceData: ServiceDTO[]) => {
+        return serviceData.map((service: ServiceDTO, index: number) => {
+            return (
+                <Route 
+                    key={index} 
+                    path={`${this.props.match.url}/${service.name.toLowerCase().replaceAll(' ', '')}`}
+                    render={(props: RouteComponentProps) => 
+                        <BillDetails 
+                            serviceDetails={this.state.billsData.services}
+                            {...props} 
+                        />
+                    }
+                />
+                )
+            })
     }
 
     render() {
         return (
             <React.Fragment>
-                <div className="bills-component" onWheel={handleHorizontalScroll}>
+                <div className="bills-component scrollable" onWheel={handleHorizontalScroll}>
                     <table className="bills-table">
-                        <thead className="table-head">
-                            <tr className="row">
-                                <th className="head">
-                                    <div className="group">
-                                        <div className="image-wrapper">
-                                            <img className="group-image" src={this.state.billsData.groupPhoto} alt="User group" />
-                                        </div>
-                                        <div className="group-info">
-                                            <h2 className="title">{this.state.billsData.groupName}</h2>
-                                            <h3 className="subtitle"> 
-                                                18.03.2021
-                                            </h3>
-                                        </div>
-                                    </div>
-                                </th>
-                                {this.renderTableHead()}
-                            </tr>
-                        </thead>
-                        <tbody className="table-body">
-                            {
-                                this.state.billsData.userBillData.map((userData: UserBillDataDTO) => {
-                                    return (
-                                        <tr className="row">
-                                            <th className="head user">
-                                                <div className="group">
-                                                    <img className="group-image" src={userData.photo} alt="User group" />
-                                                    <div className="group-info">
-                                                        <h2 className="title">{userData.name}</h2>
-                                                        <h3 className={`subtitle ${userData.paid ? 'paid' : 'unpaid'}`}> 
-                                                            {userData.paid ?
-                                                                <React.Fragment>
-                                                                    Paid 
-                                                                    <i className="status-icon fas fa-check"></i>
-                                                                </React.Fragment>
-                                                                :
-                                                                'Unpaid'
-                                                            }
-                                                        </h3>
-                                                    </div>
-                                                </div>
-                                            </th>
-                                            {userData.payments.map((payment: PaymentDTO) => {
-                                                return (
-                                                    <th className="cell">
-                                                        {payment.amount}
-                                                    </th>
-                                                )
-                                            })}
-                                            <th className="cell total">
-                                                {userData.totalAmount}
-                                            </th>
-                                        </tr>   
-                                    )
-                                })
-                            }
-                        </tbody>
+                        {this.renderTableHead()}
+                        {this.renderTableBody()}
                     </table>
                 </div>
-                {this.state.billsData.services.map((service: ServiceDTO) => {
-                    return (
-                        <Switch>
-                            <Route path={`${this.props.match.url}/${service.name.toLowerCase().replaceAll(' ', '')}`}
-                                   render={(props: RouteComponentProps) => 
-                                        <BillDetails 
-                                            serviceDetails={this.state.billsData.services}
-                                            {...props} 
-                                       />
-                                   }
-                            />
-                        </Switch>
-                    )
-                })}
+                <Switch>
+                    {this.renderBillRoutes(this.state.billsData.services)}
+                </Switch>
             </React.Fragment>
         )
     }
